8c59b5adc96f5b7f8c151033d9d5e8666af1826a
Hadrien Gourlé <gourlehadrien@gmail.com>
2017-09-06 14:00:06 +0200

    build samtools without curses

--- samtools.sh
+++ samtools.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+set -e
 # download samtools sourcecode
 wget https://github.com/samtools/samtools/releases/download/1.5/samtools-1.5.tar.bz2
 tar xjf samtools-1.5.tar.bz2
 #compile samtools
 cd samtools-1.5 || exit 1
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    ./configure
-else ./configure LDFLAGS="-static"
+    ./configure --without-curses
+else ./configure LDFLAGS="-static" --without-curses
 fi
 make
 
