8f7b920a8c48f064fb8356610181d369b0e48df4
Juliette Hayer <juliette.hayer@gmail.com>
2017-09-07 15:29:26 +0200

    antoher try gcc compiler for mac

--- megahit.sh
+++ megahit.sh
@@ -6,7 +6,7 @@ tar xvfz v1.1.2.tar.gz
 # compile with gcc
 cd megahit-1.1.2
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-    CXX=/usr/local/Cellar/gcc/*/bin/c++ make
+    CXX="/usr/local/Cellar/gcc/*/bin/g++-*" make
 else make
 fi
 
